05d86ac02cc227eefd1ab2de0c58c6c629ea5ef8
jumail j <jumailjuraige@gmail.com>
2021-09-22 12:21:04 +0530

    added camera controller

--- GamePhysics/src.cpp
+++ GamePhysics/src.cpp
@@ -30,6 +30,7 @@ int main() {
 	char ballText[40] = "";
 	char velocityVectorChar[20] = "";
 	char AcclerationChar[20] = "";
+	char massCahr[15] = "";
 
 
 	const int WindowWidht = 1200;
@@ -52,17 +53,43 @@ int main() {
 	Vector2 velocityVector{ 0,0 }; // average 
 
 	// NET FORCE;
-	netForceVector.at(0) = Vector2{ 4,0 };
-	netForceVector.at(1) = Vector2{ 1,0 };
-	netForceVector.at(2) = Vector2{ 0,2 };
+	netForceVector.at(0) = Vector2{ 0,0 };
+	netForceVector.at(1) = Vector2{ 0,0 };
+	netForceVector.at(2) = Vector2{ 0,1 };
 
 	Vector2 netForce{ 0,0 };
 	// mass;
-	float mass = 30;
+	float mass = 50;
 	Vector2 accelerationVector{ 0,0 };
 
 
+	// CAMERA SETUP
+	Camera2D camera = { 0 };
+	camera.target = Vector2{ ball.m_Position.x, ball.m_Position.y };
+	camera.offset = Vector2{ WindowWidht / 2.0f, WindowHeight / 2.0f };
+	camera.rotation = 0.0f;
+	camera.zoom = 1.0f;
+
+
+
 	while (!WindowShouldClose()) {
+		// camera Controls;
+  // Camera target follows player
+		camera.target = Vector2{ ball.m_Position.x, ball.m_Position.y };
+
+		// Camera zoom controls
+		camera.zoom += ((float)GetMouseWheelMove() * 0.05f);
+
+		if (camera.zoom > 3.0f) camera.zoom = 3.0f;
+		else if (camera.zoom < 0.1f) camera.zoom = 0.1f;
+
+		// Camera reset (zoom and rotation)
+		if (IsKeyPressed(KEY_R))
+		{
+			camera.zoom = 1.0f;
+			camera.rotation = 0.0f;
+		}
+
 
 		DeltaTime = GetFrameTime();
 		DeltaTime2.x = DeltaTime;
@@ -80,29 +107,34 @@ int main() {
 
 		accelerationVector = { netForce.x / mass, netForce.y / mass };
 		velocityVector += accelerationVector *DeltaTime2;
-		std::cout << velocityVector.x << " << " << velocityVector.y << std::endl;
 
 		// update positoin;
-		pos += (velocityVector) + netForce* DeltaTime2;
+		pos += (velocityVector)+netForce * DeltaTime2;
 		ball.SetPosition(pos);
-
-
 
 		// Drawing;
 		BeginDrawing();
+		ClearBackground(DARKPURPLE);
 
-		sprintf(ballText, "Ball pos: x = %d , y = %d", (int)pos.x-600, (int)pos.y-350);
-		DrawText(ballText, 10, 10, 30, BLACK);
+		BeginMode2D(camera); //  inCamera Control;
+		
+		ball.DrawBall();
+		
+		EndMode2D();
+
+		sprintf(ballText, "Ball pos: x = %d , y = %d", (int)pos.x-600, (int)pos.y-350); // -offset;
+		DrawText(ballText, 10, 10, 20, WHITE);
 
 		sprintf(velocityVectorChar, "Velocity: x = %.2f, y = %.2f", velocityVector.x, velocityVector.y);
-		DrawText(velocityVectorChar, 10, 35, 30, BLACK);
+		DrawText(velocityVectorChar, 10, 30, 20, WHITE);
 
 		sprintf(AcclerationChar, "Accleration: x = %.2f, y = %.2f", accelerationVector.x, accelerationVector.y);
-		DrawText(AcclerationChar, 10, 65, 30, BLACK);
+		DrawText(AcclerationChar, 10, 50, 20, WHITE);
+
+		sprintf(massCahr, "Mass= %.1f kg",mass);
+		DrawText(massCahr, 10, 70, 20, WHITE);
 
 
-		ClearBackground(DARKPURPLE);
-		ball.DrawBall();
 
 		EndDrawing();
 	}
